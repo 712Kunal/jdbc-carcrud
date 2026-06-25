a931d20ee6f933bd53f23ec3ebdb1fe613e5182b
712Kunal <jadhavkun0@gmail.com>
2026-06-26 01:12:55 +0530

    added project

---
+++ src/main/java/com/jsp/carcrud/CarDriver.java
@@ -0,0 +1,70 @@
+package com.jsp.carcrud;
+
+import java.util.Scanner;
+
+public class CarDriver {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		boolean flag = true;
+
+		while (flag) {
+			System.out.println("\nCAR'S OPERATIONS ->");
+			System.out.println("1. INSERT A CAR");
+			System.out.println("2. UPDATE THE PROPERTY OF A CAR");
+			System.out.println("3. DELETE THE PROPERTY OF A CAR");
+			System.out.println("4. READ THE PROPERTIES OF A CAR");
+			System.out.println("5. EXIT");
+			System.out.println();
+
+			System.out.println("\nWhich operation do you want to perform?");
+
+			int choice = sc.nextInt();
+
+			switch (choice) {
+
+			case 1: {
+				CarRepo.insertCar(sc);
+				System.out.println("\nData added successfully!!!!");
+				break;
+			}
+
+			case 2: {
+				CarRepo.updateCar(sc);
+				System.out.println("\nData updated successfully!!!!");
+				break;
+			}
+
+			case 3: {
+				CarRepo.deleteCar(sc);
+				System.out.println("\nData deleted successfully!!!!");
+				break;
+			}
+
+			case 4: {
+				CarRepo.readCar(sc);
+				System.out.println("\nData fetched successfully!!!!");
+				break;
+			}
+			case 5: {
+				System.out.println("5. Do you want to exit the program? \n1. YES \n2. NO");
+				int id = sc.nextInt();
+
+				if (id == 1) {
+					sc.close();
+					CarRepo.close();
+					flag = false;
+				} else {
+					continue;
+				}
+				break;
+			}
+
+			default: {
+				System.out.println("Invalid Choice!");
+				break;
+			}
+			}
+		}
+	}
+}
---
+++ src/main/java/com/jsp/carcrud/CarRepo.java
@@ -0,0 +1,147 @@
+package com.jsp.carcrud;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class CarRepo {
+	static Connection conn;
+
+	static {
+		try {
+			Class.forName("org.postgresql.Driver");
+
+			Dotenv dotenv = Dotenv.load();
+
+			String url = dotenv.get("DB_URL");
+			String username = dotenv.get("DB_USERNAME");
+			String password = dotenv.get("DB_PASSWORD");
+
+			conn = DriverManager.getConnection(url, username, password);
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void insertCar(Scanner sc) {
+		try {
+			String insertQuery = "INSERT INTO car VALUES(?,?,?,?,?,?,?)";
+			PreparedStatement st = conn.prepareStatement(insertQuery);
+
+			System.out.println("ENTER THE CAR'S DATA ->");
+			System.out.print("Enter the Car ID: ");
+			int id = sc.nextInt();
+
+			System.out.print("Enter the Car Brand: ");
+			String brand = sc.next();
+
+			System.out.print("Enter the Car model: ");
+			String model = sc.next();
+
+			System.out.print("Enter the Car color: ");
+			String color = sc.next();
+
+			System.out.print("Enter the Car price: ");
+			double price = sc.nextDouble();
+
+			System.out.print("Enter the number of seats: ");
+			int seats = sc.nextInt();
+
+			System.out.print("Enter the avg of the car: ");
+			double avg = sc.nextDouble();
+
+			st.setInt(1, id);
+			st.setString(2, brand);
+			st.setString(3, model);
+			st.setString(4, color);
+			st.setDouble(5, price);
+			st.setInt(6, seats);
+			st.setDouble(7, avg);
+
+			st.execute();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void updateCar(Scanner sc) {
+		try {
+			String updateQuery = "UPDATE car SET color=? WHERE id=?";
+			PreparedStatement st = conn.prepareStatement(updateQuery);
+
+			System.out.println("ENTER THE DATA FOR THE UPDATION ->");
+			System.out.print("Enter the id of the car to update: ");
+			int id = sc.nextInt();
+			System.out.print("Enter the color of the car to updated: ");
+			String color = sc.next();
+
+			st.setString(1, color);
+			st.setInt(2, id);
+
+			st.execute();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void deleteCar(Scanner sc) {
+		try {
+			String deleteQuery = "DELETE FROM car WHERE id=?";
+			PreparedStatement st = conn.prepareStatement(deleteQuery);
+
+			System.out.println("ENTER THE DATA FOR THE DELETION ->");
+			System.out.print("Enter the car's id to delete: ");
+			int id = sc.nextInt();
+
+			st.setInt(1, id);
+			st.execute();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void readCar(Scanner sc) {
+		try {
+			String readQuery = "SELECT * FROM car";
+			PreparedStatement st = conn.prepareStatement(readQuery);
+			st.execute();
+			ResultSet rs = st.getResultSet();
+
+			while (rs.next()) {
+				int id = rs.getInt(1);
+				String brand = rs.getString(2);
+				String model = rs.getString(3);
+				String color = rs.getString(4);
+				double price = rs.getDouble(5);
+				int seats = rs.getInt(6);
+				double avg = rs.getDouble(7);
+
+				System.out.println(id + " | " + brand + " | " + model + " | " + color + " | " + price + " | " + seats
+						+ " | " + avg);
+
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void close() {
+		try {
+			conn.close();
+			System.out.println("Program exited successfully!!!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
